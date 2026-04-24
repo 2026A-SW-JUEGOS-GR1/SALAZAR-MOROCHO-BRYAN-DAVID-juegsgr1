23a60da90fec54375741a8e9576c1479f8673efa
Bryan David Salazar Morocho <salazar.bryan@epn.edu.ec>
2026-04-24 18:28:27 -0500

    Update project files and assets

---
+++ 00-2d-/spritesheet-tiles-default.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="spritesheet-tiles-default" tilewidth="32" tileheight="32" tilecount="1296" columns="36">
+ <image source="../Downloads/kenney_new-platformer-pack-1.1/Spritesheets/spritesheet-tiles-default.png" width="1169" height="1169"/>
+</tileset>
